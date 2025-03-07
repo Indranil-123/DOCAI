6beb41194d42c38e54b8d5b2950a377559b03373
Indranil Bakshi <officialindranilbakshi@gmail.com>
2025-03-07 13:16:32 +0530

    neel commits

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,41 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Doctors from './pages/Doctors'
 import Login from './pages/Login'
 import About from './pages/About'
-import Appointments from './pages/Appointments'
-import My_Profile from './pages/My_Profile'
-import Navbar from './components/Navbar'
+import Contact from './pages/Contact'
+import Appointment from './pages/Appointment'
+import MyAppointments from './pages/MyAppointments'
+import MyProfile from './pages/MyProfile'
+import Footer from './components/Footer'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Verify from './pages/Verify'
 import Ai from './pages/Ai'
 import Prediction from './pages/Prediction'
 
-function App() {
+const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
-      <Navbar/>
+      <ToastContainer />
+      <Navbar />
       <Routes>
-        <Route path ='/' element={<Home />}/>
-        <Route path = '/doctors' element={<Doctors/>}/>
-        <Route path = '/doctors/:speciality' element={<Doctors/>}/>
-        <Route path = '/login' element={<Login/>}/>
-        <Route path = '/about' element={<About/>}/>
-        <Route path ='/appointments' element={<Appointments/>}/>
-        <Route path ='/appointments:docId' element={<Appointments/>}/>
-        <Route path='/ai' element={<Ai/>} />
-        <Route path ='/profile' element = {<My_Profile/>}/>
-        <Route path='/prediction' element={<Prediction/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:speciality' element={<Doctors />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/appointment/:docId' element={<Appointment />} />
+        <Route path='/my-appointments' element={<MyAppointments />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/verify' element={<Verify />} />
+        <Route path ='/ai' element={<Ai/>}/>
+        <Route path ='/prediction' element={<Prediction/>}/>
       </Routes>
+      <Footer />
     </div>
   )
 }
